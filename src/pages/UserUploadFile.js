2d7aa667431bd9f7c84d65d7e94da26a6097e525
akramnarejo <ak.narejo@gmail.com>
2023-08-20 22:43:59 +0500

    fix: file upload UI

--- src/pages/UserUploadFile.js
+++ src/pages/UserUploadFile.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import {storage } from "src/firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { addDoc, collection } from "firebase/firestore";
 import { useUserAuth } from "src/context";
+import { useStore } from "src/store";
+import { shallow } from "zustand/shallow";
+import { Input } from '@mui/material';
+import { LoadingButton } from "@mui/lab";
 
-function FileUpload({ userInfo, firebaseApp }) {
+function FileUpload() {
+  const { userInfo, setNotify } = useStore(
+    (state) => ({
+      userInfo: state?.userInfo,
+      setNotify: state?.setNotify,
+    }),
+    shallow
+  );
+  const fileRef = useRef(null)
   const { db } = useUserAuth();
   const [file, setFile] = useState(null);
   const [filename, setFilename] = useState("");
+  const [loading, setLoading] = useState(false)
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
@@ -20,6 +33,7 @@ function FileUpload({ userInfo, firebaseApp }) {
   };
 
   const handleFileUpload = async () => {
+    setLoading(true)
     if (!file || filename.trim() === "") {
       console.error("Please select a file and enter a filename.");
       return;
@@ -37,37 +51,49 @@ function FileUpload({ userInfo, firebaseApp }) {
     const fileDetails = {
       name: filename,
       url: downloadURL,
-      organization: userInfo.organization,
-      reservedOrg: userInfo.reservedOrg,
-      createdBy: userInfo.email,
+      organization: userInfo?.organization ?? null,
+      reservedOrg: userInfo?.reservedOrg ?? null,
+      createdBy: userInfo?.email ?? null,
     };
 
     // Save the file details to Firestore
     try {
+      console.log("-------------details: ", fileDetails, userInfo)
       await addDoc(collection(db, "files"), fileDetails);
+      setNotify({ open: true, message: 'File uploaded successfully!', type: 'success' })
+      fileRef.current.value = null
+      setFile(null)
+      setFilename("")
+      setLoading(false)
       console.log("File details saved to Firestore");
     } catch (error) {
+      setLoading(false)
+      setNotify({ open: true, message: "Failed to upload!", type: 'error' })
       console.error("Error saving file details:", error);
     }
   };
 
   return (
-    <div>
-      <input
+    <>
+    <h2>Upload File</h2>
+    <div style={{display: 'flex', flexDirection: 'column', gap: 4, width: '20rem'}}>
+      <Input
         type="file"
         accept=".pdf,.doc,.docx,.txt"
         onChange={handleFileChange}
+        ref={fileRef}
       />
       <br />
-      <input
+      <Input
         type="text"
         placeholder="Enter filename"
         value={filename}
         onChange={handleFilenameChange}
       />
       <br />
-      <button onClick={handleFileUpload}>Upload File</button>
+      <LoadingButton onClick={handleFileUpload} variant="contained" loading={loading}>Upload File</LoadingButton>
     </div>
+    </>
   );
 }
 
